eac2141e35a3bc967a67706f35e74adeb66c3a8c
abedala <patrickabedala@hotmail.com>
2024-12-16 14:48:57 -0300

    correção bugs

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,28 +1,39 @@
 import { Component } from '@angular/core';
 import { LoginLayoutComponent } from '../../components/login-layout/login-layout.component';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Form, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 import { Router } from '@angular/router';
 import { LoginServicesService } from '../../services/login-services.service';
 import { ToastrService } from 'ngx-toastr';
 
+interface loginForm{
+email:FormControl,
+password: FormControl
+}
+
 @Component({
   selector: 'app-login',
+  standalone: true,
+
   imports: [
     LoginLayoutComponent,
     ReactiveFormsModule,
     PrimaryInputComponent
   ],
+
+  providers: [ LoginServicesService],
+
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  [x: string]: any;
 loginForm!: FormGroup;
-  toastService: any;
-
+  
   constructor(
     private router: Router,
-    private loginService: LoginServicesService
+    private loginService: LoginServicesService,
+    private  toastService: ToastrService
   ){
     this.loginForm = new FormGroup({
       email: new FormControl('', [Validators.required, Validators.email]),
@@ -33,8 +44,8 @@ loginForm!: FormGroup;
   }
   submit(){
     this.loginService.login(this.loginForm.value.email,  this.loginForm.value.password).subscribe({
-      next: () => this.toastService.success("Login feito com sucesso"),
-      error: () =>this.toastService.error("error inesperado! Tente novamente mais tarde")
+      next: () => this['toastService'].success("Login feito com sucesso"),
+      error: () =>this['toastService'].error("error inesperado! Tente novamente mais tarde")
     })
       
     
